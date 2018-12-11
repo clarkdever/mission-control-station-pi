60f19f3aa144e57829a2c97f871d41234ea5611b
Clark Dever <clarkdever@gmail.com>
2018-12-10 22:36:05 -0500

    Add hack to cancel timers and allow clips to play for duration

--- video-player.py
+++ video-player.py
@@ -12,38 +12,42 @@ mplayer -input cmdlist will provide you a list of commands
 IMPORTANT NOTE: The mplayer wrapper used here will only implement commands if there is not a property available that does the same thing
 
 TODO:
-1) Test on pi to see if mplayer loads videos fast enough
-2) Figure out how to pause after a duration if it's not
-2.1) If pausing after duration, reset time after each button press
-3) implement with arcade buttons instead of keylisteners
+1) implement with arcade buttons instead of keylisteners
 """
 
+#initialize all the things
 p = mplayer.Player()
-p.loadfile('sample.mp4')
-p.fullscreen = True
-p.ontop = True
-print('pause: ', p.pause)
-p.pause = True
-print('pause: ', p.pause)
-print('file name:', p.path, "\n", 'clip length:', p.length)
+p.loadfile('sharks.mp4')
+p.fullscreen = True #make it fullscreen
+p.ontop = True #place our video layer on top
+p.pause()  #unpause the file, it loads paused
+p.osdlevel = 0 #turn off on screen display
+#print('file name:', p.path, "\n", 'clip length:', p.length)
 
 
 def play_clip(position, duration):
-    """Plays a clip starting at position for duration"""
+    """Plays a clip starting at position for duration
+    This is really hacky - please tell me how to do it correctly.
+    """
     print('play_clip()')
     try:
-        t = Timer(duration, p.pause())
+        if t is not None: #if a timer exists, cancel the timer before creating a new one
+            t.cancel()
+            t = Timer(duration, pause)
+            p.time_pos = position
+            t.start()
+    except NameError: #this is the first time this method has been called, create a timer
+        global t 
+        t = Timer(duration, pause)
         p.time_pos = position
         t.start()
-    except:
-        print('Play_Clip Exception {0} , {1}')
+
 
 def pause():
     """Pauses play"""
-    print('pause()')
     try:
         p.pause()
-        print('Pause()')
+        print('pause() fired by play_clip Timer')
     except:
         print('Pause Failed')
         
@@ -65,6 +69,36 @@ def on_press(key):
             p.fullscreen = True
         elif key.char=='f':
             p.fullscreen = not p.fullscreen
+        elif key.char=='p':
+            print('toggle pause')
+            p.pause()
+        elif key.char=='z':
+            print('data dump')
+            print('path: ', p.path)
+            #print('stream_pos: ', p.stream_pos)
+            #print('stream_start: ', p.stream_start)
+            #print('stream_end: ', p.stream_end)
+            #print('stream_length: ', p.stream_length)
+            #print('stream_time_pos: ', p.stream_time_pos)
+            print('length: ', p.length)
+            print('time_pos: ', p.time_pos)
+            print('percent_pos: ', p.percent_pos)
+        elif key.char=='q':
+            print('Sharks!')
+            play_clip(5.75, 2)
+            #p.time_pos = 5.75
+        elif key.char=='w':
+            print('Tiger Shark!')
+            play_clip(20.55, 2)
+            #p.time_pos = 20.55
+        elif key.char=='e':
+            print('Blue Fin Shark!!')
+            #p.time_pos = 35
+            play_clip(35, 2)
+        elif key.char=='r':
+            print('Thresher Shark!')
+            #p.time_pos = 51
+            play_clip(51, 2)
         else:
             print('alphanumeric key {0} pressed'.format(
                 key.char))
