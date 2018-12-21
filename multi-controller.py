d98926a9221fbf7aa89f61e6cda60c9bba7e1cfe
Clark Dever <clarkdever@gmail.com>
2018-12-20 22:57:13 -0500

    Add event printing to  multi-controller.py

--- multi-controller.py
+++ multi-controller.py
@@ -5,7 +5,8 @@ player2 = evdev.InputDevice('/dev/input/event5')
 
 async def print_events(device):
     async for event in device.async_read_loop():
-        print(device.path, evdev.categorize(event), sep=': ')
+        if event.type == evdev.ecodes.EV_KEY:
+            print(evdev.categorize(event))
 
 for device in player1, player2:
     asyncio.ensure_future(print_events(device))
